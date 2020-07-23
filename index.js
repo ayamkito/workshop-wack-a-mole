e5b0c92fd5149d8b30aa4dbb22fe11c2aa721f31
akwotiga kizito <kakwotiga@yahoo.com>
2020-07-23 00:13:43 -0400

    almost done

--- index.js
+++ index.js
@@ -3,4 +3,20 @@
 let score = 0;
 var scoreDisplay = document.getElementById('score');
 
-console.log(scoreDisplay);
+// console.log(scoreDisplay);
+
+var repeatition = setInterval(timer, 3000)
+function timer(){
+    var moleHole = Array.from(document.getElementsByClassName("hole"))
+    var randomHoleIndex = Math.floor(Math.random()*Math.floor(moleHole.length))
+    moleHole[randomHoleIndex].classList.toggle("mole")
+}
+
+var whackMole = document.getElementById("whack-a-mole")
+whackMole.addEventListener("click", function(event) {
+  if(event.target.matches(".mole")){
+    console.log(true)
+    score++
+  }
+})
+console.log(score)
